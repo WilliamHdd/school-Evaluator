6785748e69ef1afff71457296162b18dfe1dfc1e
Mathieu David <mathieudavid@mathieudavid.org>
2016-10-25 10:59:15 +0200

    Split menu creation into its own class

--- students/EvaluatorApp.cs
+++ students/EvaluatorApp.cs
@@ -11,55 +11,31 @@ namespace Evaluator
 			EvaluatorApp.InitMenu ();
 			EvaluatorApp.MainMenu ();
 		}
-
-		private static void ConstructMenu (Tuple<string, Func<bool>>[] options) {
-			while (true) {
-				for (int i = 0; i < options.Length; i++) {
-					Console.WriteLine ("\t" + (i + 1) + ") " + options [i].Item1);
-				}
-
-				int choice;
-
-				try {
-					choice = Int32.Parse (Console.ReadLine ()) - 1;
-					if (choice >= 0 || choice < options.Length) {
-						if (options [choice].Item2 ()) {
-							break;
-						} else {
-							Console.WriteLine ("Could not perform the operation... :'(");
-						}
-					}
-				} catch (FormatException) {
-					// Do nothing, the range and parse error is handled at the same time below
-				}
-
-				Console.WriteLine ("Invalid choice...");
-			}
-		}
-
+			
 		private static void InitMenu () {
 
 			Console.WriteLine ("Welcome, would you like to create a new establishment or import an existing one (from file)\n");
 
-			Tuple<string, Func<bool>>[] options = {
-				new Tuple<string, Func<bool>> ("Create new", EvaluatorApp.NewEstablishment),
-				new Tuple<string, Func<bool>> ("Create new", EvaluatorApp.ImportEstablishment)
-			};
+			Menu init_menu = new Menu ();
 
-			EvaluatorApp.ConstructMenu (options);
+			init_menu
+				.add_option ("Create new", EvaluatorApp.NewEstablishment)
+				.add_option ("Import", EvaluatorApp.ImportEstablishment);
+
+			init_menu.Run ();
 		}
 
 		private static void MainMenu () {
 			Console.WriteLine ("What would you like to do?\n");
 
-			Tuple<string, Func<bool>>[] options = {
-				new Tuple<string, Func<bool>> ("Manage students", EvaluatorApp.Dummy),
-				new Tuple<string, Func<bool>> ("Manage teachers", EvaluatorApp.Dummy),
-				new Tuple<string, Func<bool>> ("Manage courses", EvaluatorApp.Dummy)
-			};
+			Menu main_menu = new Menu ();
 
-			EvaluatorApp.ConstructMenu (options);
+			main_menu
+				.add_option ("Manage students", EvaluatorApp.Dummy)
+				.add_option ("Manage teachers", EvaluatorApp.Dummy)
+				.add_option ("Manage courses", EvaluatorApp.Dummy);
 
+			main_menu.Run ();
 		}
 
 		private static bool NewEstablishment () {
@@ -77,23 +53,6 @@ namespace Evaluator
 				Console.WriteLine ("An error has occured, please contact the creator of this app.");
 			}
 			return true;
-		}
-
-		private static int WaitForChoice (int min, int max) {
-			while (true) {
-				int choice;
-
-				try {
-					choice = Int32.Parse (Console.ReadLine ());
-					if (choice > min || choice < max) {
-						return choice;
-					}
-				} catch (FormatException) {
-					// Do nothing, the range and parse error is handled at the same time below
-				}
-
-				Console.WriteLine ("Invalid choice...");
-			}
 		}
 
 		private static bool Dummy () {
---
+++ students/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Evaluator
+{
+	public class Menu
+	{
+		private List<Tuple<string, Func<bool>>> options;
+
+		public Menu () {
+			this.options = new List<Tuple<string, Func<bool>>> ();
+		}
+
+		public Menu add_option(string text, Func<bool> action) {
+			this.options.Add (new Tuple<string, Func<bool>> (text, action));
+			return this;
+		}
+
+		public void Run() {
+			while (true) {
+				for (int i = 0; i < this.options.Count; i++) {
+					Console.WriteLine ("\t" + (i + 1) + ") " + options [i].Item1);
+				}
+
+				int choice;
+
+				try {
+					choice = Int32.Parse (Console.ReadLine ()) - 1;
+					if (choice >= 0 || choice < this.options.Count) {
+						if (options [choice].Item2 ()) {
+							break;
+						} else {
+							Console.WriteLine ("Could not perform the operation... :'(");
+						}
+					}
+				} catch (FormatException) {
+					// Do nothing, the range and parse error is handled at the same time below
+				}
+
+				Console.WriteLine ("Invalid choice...");
+			}
+		}
+	}
+}
+
